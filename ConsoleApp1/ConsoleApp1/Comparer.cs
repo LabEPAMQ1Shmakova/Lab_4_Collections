043b373012188f6c7b3196bf698bdae550129e16
juksonya <juksonya@gmail.com>
2018-04-23 02:32:08 +0300

    new com

---
+++ ConsoleApp1/ConsoleApp1/Comparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4_Collections
+{
+    class Comparer : IComparer<OilProducts>
+    {
+        /*public int Compare(OilProducts x , OilProducts y)
+        {
+            if (x.volume > y.volume)
+                return 1;
+            else if (x.volume < y.volume)
+                return -1;
+            else
+                return 0;
+        }*/
+      
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Collections
+{
+    class Prog
+    {
+        static void Main(string[] args)
+        {
+            Kerosene Kerosene = new Kerosene(100, 50, "Керосин ТС-1");
+            Kerosene.Dencity();
+            Petrol Petrol = new Petrol(100, 75, "Автомобильный бензин АИ-95");
+            Petrol.Dencity();
+            DieselFluel DieselFluel = new DieselFluel(100, 85, "Дизельное топливо ДТ62");
+            DieselFluel.Dencity();
+
+                       
+
+            List<string> products = new List<string>();
+            products.Add("DieselFluel");
+            products.Add("Petrol");
+            products.Add("Kerosene");
+
+
+           products.Sort();
+            
+            foreach (string str in products) ;
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+
+
+        }
+        
+    }
+
